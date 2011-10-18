b23c923da3e91880b6eba5228b038480ca973ecc
Mikael Svensson <github@mikaelsvensson.info>
2011-10-18 20:18:35 +0200

    Chrome image region selection.

---
+++ webcontent/viewer/viewer-annotations.js
@@ -0,0 +1,28 @@
+/**
+ * @author Mikael
+ */
+
+(function($) {
+
+	CommentAnnotation = function (text, author) {
+		this.text = text;
+		this.author = author;
+	}
+	BalloonAnnotation = function (text, author, coord) {
+		this.text = text;
+		this.author = author;
+		this.coord = coord;
+	}
+	MosaicAnnotation = function (text, author, region) {
+		this.text = text;
+		this.author = author;
+		this.region = region;
+	}
+	StampAnnotation = function (text, author, region, shape) {
+		this.text = text;
+		this.author = author;
+		this.region = region;
+		this.shape = shape;
+	}
+
+})(jQuery);
