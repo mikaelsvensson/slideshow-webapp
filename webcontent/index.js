d02530d31f4c43673891fe837de9e238d0060062
Mikael Svensson <github@mikaelsvensson.info>
2011-10-22 21:41:43 +0200

    Reorganized folders.

--- webcontent/index.js
+++ webcontent/index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
 	var U = new Utils();
-	
+
 	try {
 		/*
 		 * HTML5: Web Storage
@@ -9,73 +9,45 @@ window.onload = function() {
 		var slideshowModel = JSON.parse(localStorage["slideshow-webapp-model"]);
 	} catch (ex) {
 	}
-	if (!slideshowModel) {
+	if(!slideshowModel) {
 		var slideshowModel = {
 			title : "Untitled Slideshow",
 			slides : []
 		};
 	}
-/*
-	U.xhrJson("http://www.mikaelsvensson.info/slideshow/service.php?request=images-list", true
-	, function(data) {
-		
-		var imageCollection = new ImageCollection();
-		
-		for(var i in data) {
-			
-			imageCollection.addImage(
-				data[i].url, 
-				data[i].thumbnailUrl, 
-				data[i].width, 
-				data[i].height
-			);
-		}
-		
-		var viewer = new SlideshowViewer(slideshowModel, imageCollection, "viewer-slide", "viewer-prev-button", "viewer-next-button", "viewer-annotations-buttons", "viewer-close-button", "viewer-comments");
-		viewer.init();
-	
-		var designer = new SlideshowDesigner(slideshowModel, imageCollection, viewer, "designer-images", "designer-availableimages", "designer-gallery-droparea", "designer-buttons");
-		designer.init();
-	});
-*/
-  socket = io.connect('http://localhost:8080/');
-  socket.on('connect', function () {
-    socket.send('hi');
-    
-    socket.emit("login", { name: prompt("Vad heter du?") } );
-    
-    socket.emit("image-list", null);
-    
-    socket.on("image-list-response", function (data) {
-		var imageCollection = new ImageCollection();
-		
-		for(var i in data) {
-			
-			imageCollection.addImage(
-				data[i].url, 
-				data[i].thumbnailUrl, 
-				data[i].width, 
-				data[i].height
-			);
-		}
-		
-		var viewer = new SlideshowViewer(slideshowModel, imageCollection, "viewer-slide", "viewer-prev-button", "viewer-next-button", "viewer-annotations-buttons", "viewer-close-button", "viewer-comments");
-		viewer.init();
-	
-		var designer = new SlideshowDesigner(slideshowModel, imageCollection, viewer, "designer-images", "designer-availableimages", "designer-gallery-droparea", "designer-buttons");
-		designer.init();
-    });
+	socket = io.connect('http://localhost:8080/');
+	socket.on('connect', function() {
+		socket.send('hi');
 
-    socket.on('userlist-updated', function (clientList) {
-      console.log("Clients: " + clientList);
-    });
-    
-    socket.on('message', function (msg) {
-      console.log("Received " + msg);
-    });
-  });
-  
-  
+		socket.emit("login", {
+			name : prompt("Vad heter du?")
+		});
+
+		socket.emit("image-list", null);
+
+		socket.on("image-list-response", function(data) {
+			var imageCollection = new ImageCollection();
+
+			for(var i in data) {
+
+				imageCollection.addImage(data[i].url, data[i].thumbnailUrl, data[i].width, data[i].height);
+			}
+
+			var viewer = new SlideshowViewer(slideshowModel, imageCollection, "viewer-slide", "viewer-prev-button", "viewer-next-button", "viewer-annotations-buttons", "viewer-close-button", "viewer-comments");
+			viewer.init();
+
+			var designer = new SlideshowDesigner(slideshowModel, imageCollection, viewer, "designer-images", "designer-availableimages", "designer-gallery-droparea", "designer-buttons");
+			designer.init();
+		});
+
+		socket.on('userlist-updated', function(clientList) {
+			console.log("Clients: " + clientList);
+		});
+
+		socket.on('message', function(msg) {
+			console.log("Received " + msg);
+		});
+	});
 
 	U.$("designer-title").innerHTML = slideshowModel.title;
 
@@ -86,7 +58,6 @@ window.onload = function() {
 		localStorage["slideshow-webapp-model"] = JSON.stringify(slideshowModel);
 	};
 };
-window.onunload = function () {
-	alert("Bye");
+window.onunload = function() {
 	socket.emit("logout");
 };
